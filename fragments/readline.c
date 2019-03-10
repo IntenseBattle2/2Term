77a9ac0e2151387a6dc32fd5c6065e0c1ede5f6b
ColinTNMP <IB2Member01@gmail.com>
2019-03-09 19:54:55 -0500

    Moved data containers in readline.c to datastore.c

--- fragments/readline.c
+++ fragments/readline.c
@@ -49,7 +49,21 @@
  * categorize equations.
 */
 
-
+/* File dependencies:
+ *
+ * datastore.c
+ * |struct Instruction
+ * ||struct Variable
+ * |||enum DataType
+ * |||union Content
+ * |struct Variable
+ * ||enum DataType
+ * ||union Content
+ * |struct Macro
+ * ||struct Variable
+ * |||enum DataType
+ * |||union Content
+*/
 
 /*
  * ||================||
@@ -71,123 +85,6 @@ const char* preProcInstructions[11] = {
   "define", "include", "undef", "ifdef",
   "ifndef", "if",      "else",  "elif",
   "endif",  "error",   "pragma"         };
-
-
-
-/*
- * ||=================||
- * || Building Blocks ||
- * ||=================||
-*/
-
-//For various data types. Basically any of the keywords that can be used on variables, functions, structs, etc
-enum DataType {
-  typeAuto, typeSigned = 0,
-  typeConst,
-  typeUnsigned = 2,
-  typeStatic   = 4,
-  typeLong     = 8,
-  typeChar     = 16,
-  typeShort    = 32,
-  typeInt      = 64,
-  typeDLong    = 128,
-  typeFloat    = 256,
-  typeDouble   = 512,
-  typeVoid     = 1024
-};
-
-
-//For variable content. Encapsulates all common types. Arrays are created as arrays of Content unions.
-union Content {
-  signed   char        schar;
-  unsigned char        uchar;
-  signed   short       sshort;
-  unsigned short       ushort;
-  signed   int         sint;
-  unsigned int         uint;
-  signed   long        slong;
-  unsigned long        ulong;
-  signed   long long   sdlong;
-  unsigned long long   udlong;
-  signed   float       sfloat;
-  unsigned float       ufloat;
-  signed   long float  slfloat;
-  unsigned long float  ulfloat;
-  signed   double      sdouble;
-  unsigned double      udouble;
-  signed   long double sldouble;
-  unsigned long double uldouble;
-};
-
-
-//Variable structures, for keeping track of variables for the intepreted C code. Using `struct Variable*` will make a pointer structure.
-struct Variable {
-  char            name[999] = "nul";
-  enum   DataType type = typeVoid;
-  struct Content  value;
-};
-
-
-//Contains all the necessary data of a macro / macro function.
-struct Macro {
-  char            name[999] = "NUL";
-  struct Variable args[999];      //999 max args. NOTE: default struct setup, `void nul`, means "not a function macro" AS LONG AS args[0] IS THIS
-  char            value[999,999]; //[line,char]; 999 lines of 999 chars
-};
-
-
-/*
- * NOTE:  This struct (`struct Instruction`) will have to change to fit the "math-centric" changes. Mainly, everything must more-or-less default to `performMath`, meaning a removal of this flag in favor for a
- *        "non-math" counterpart. Among this, instructions must be able to call other instructions, as sometimes a mathematical equation (especially in conditionals and the like) may require the result of another
- *        instruction.
- *
- *        On top of these, a few glaring issues have been noticed regarding instructions. The first of which is the most important and must be fixed before proceeding with any other parts of >Terminal_. This issue
- *        is that instructions cannot call native functions. After all, some of the most important functions are ones such as `malloc()`, `realloc()`, `free()`, `syscall()`, et al. Without these, much of C would break.
- *        One thing to note is that this may not mean any sturctural changes to `Instruction` specifically for these calls. After all, being able to seamlessly connect native code to non-native code is preferred. For
- *        example, if an instruction can use math to call a function pointer that points to the address of one of these functions, then that would work out perfectly. However, that would most likely mean a change with
- *        variables.
- *
- *        The other issue, which should be addressed before proceeding with any other parts of >Terminal_, is the fact that code blocks (ie, code surrounded in { ... }) cannot be easily created. The main issue that makes
- *        this a bit harder to deal with is the fact that not only can code blocks be used with `for`, `if`, and similar statements, but they can also be used completely by themselves! while any cases of `for`, `while`, etc
- *        can be grouped as a function with names like `for1`, `while3`, `if3else`, etc, blank code blocks wouldn't be able to use these well unless there's a similar setup, such as `block4` or similar. Even then, these blocks
- *        would then require a return type (which could be void), possible arguments (which could also be voided), etc
- *
- *        One method that could get rid of both problems has been devised, but currently it is unknown whether or not this course of action will be taken. To start, a new structure, `struct CodeBlock`, would have to be
- *        created. These would be similar to `Function` structures as they could contain a return type, however, it won't take arguments or have a name. Instead, `Function` will be merged with `Variable`, the difference
- *        between the two decided by if the member for the code block is empty or not. If it is, then it's a variable. If not, then it's a function. On top of this, the structure `Conditional` will be created, which will have
- *        mathematical equations has conditions (provided by an array) and an array of code blocks that matches up with the possible conditions. There may be up to 99 conditions including `else` / `default`, thus up to 99 code
- *        blocks as well. Macros will also be fit to match the new `Variable` structure. Loops will be considered code blocks with extra instructions at the start/end depending on what type of loop (generally, these will be
- *        if statements or math, such as variable assignemnts). Code blocks, despite being multiple instructions, can be put in an instruction slot as one instruction. They will contain there own scope as well, meaning they
- *        will keep track of variables created in them and destroy them when exiting unless the variables are registered as `static`.
-*/
-//Contains all necessary data of a single instruction.
-//An instruction is either a C command via keyword, function call, or variable/macro stuff (AKA math)
-struct Instruction {
-  enum {
-    CPP_Code    = 1,  //CPP = C Pre-Processor
-    flowControl = 2,  //Flow controls = C commands. This is the "keyword is involved" flag.
-    createData  = 4,  //This instruction creates data, be it a variable, macro, function, or alike.
-    deleteData  = 8,  //This instruction deletes data, be it a variable, macro, function, or alike.
-    performMath = 16, //This variable performs a mathematical operation. Standard and will probably be removed in favor of a "calles another instruction" flag.
-  } InstructionFlags;
-
-  struct Variable involvedVars[999];   //For var creation
-  struct Function involvedFuncs[999];  //For func creation
-  struct Macro    involvedMacros[999]; //For macro creation
-  char            involvedMath[999];   //For math, obviously
-  short           involvedKeyword;     //Whatever the keyword is (if any, eg "if", "return", or "while"), but by its index number in keywords[]
-};
-
-
-//Contains all necessary data of a single function. A function in this case is a "package" of multiple instructions executed in succession.
-struct Function {
-  char               name[999] = "NUL";
-  enum   DataType    returnValue = typeVoid;
-  struct Variable    args[999];   //Note that if an entry == `void nul`, then that means that's the end; no more arguments; REGARDLESS of what's passed that point!
-  struct Instruction code[999];   //Instructions are considered to end first instance of an empty instruction (0, [0]=='void nul', [0]=='void NUL(void)', [0]=='NUL', <empty>, 0) REGARDLESS OF WHAT'S PASSED THAT POINT!
-};
-
-
 
 /*
  * ||===========||
--- fragments/runInstruction.c
+++ fragments/runInstruction.c
@@ -1,6 +1,6 @@
 // >Terminal_ / 2Term - dual "shell" and language interpreter
 // Copyright (C)2019 Intense Battle 2
-// This code file is licensed under the GNU GPL. Please read the begiining comments of this program's main.c file for more information.
+// This code file is licensed under the GNU GPL. Please read the beginning comments of this program's main.c file for more information.
 /*
  * About this file: runInstruction.c
  *
@@ -12,76 +12,11 @@
  * structures, running functions, and performing math.
  *
  * `runInstruction()` does not handle pre-processor
- * instructions; instead, it queries the preprocessor
- * function for these tasks should it ever be handed
- * such data, though the pre-processor should have
- * already handled such requests during the processing
- * of code lines into Instruction structs.
+ * instructions as these do not have a stored state and
+ * should have already been handled.
  *
- * Here, it should be stated what an instruction counts
- * as. In C, we think of an instruction as any
- * statement that either declares or defines data,
- * calls a function, or uses any of the given keywords.
- * These may or may not end in a semicolon, and may or
- * may not perform math. They also may or may not have
- * code blocks (ie, instructions wrapped in braces)
- * attached to them. Some examples of single
- * instructions are below:
- *
- * `num = 4 * num2 + (num < num2);`
- *
- * `someFunction();`
- *
- * ```
- * if( num == 28 )
- * {
- *   num3 = anotherFunction(num2);
- * }
- * ```
- *
- * Each of these instructions are different. One of
- * them even contains other instructions. However, they
- * all count as one, more or less. It should be noted
- * that some resolve to calling other instructions, and
- * so could resolve to multiple in that sense. However,
- * they all resolve to variable amounts of steps. For
- * example, here are the steps broken down from the
- * previous instructions:
- *
- *
- * `num = 4 * num2 + (num < num2);`
- *   check if var 'num' is less than var 'num2' [r0]
- *   multiply 4 by var 'num2' [r1]
- *   add result 'r0' to result 'r1' [r3]
- *   assign result 'r3' to var 'num'
- *
- * `someFunction();`
- *   retrieve instruction list tied to 'someFunction'
- *     and run with argument list '[]'
- *
- * ```
- * if( num == 28 )
- * {
- *   num3 = anotherFunction(num2);
- * }
- * ```
- *   check if var 'num' equals 28 [r0]
- *   if result 'r0' resolves to true, then run
- *     instruction list below:
- *       retrieve instruction list tied to
- *         'anotherFunction' and run with argument list
- *         '[var 'num']' [r1]
- *       assign result 'r1' to var 'num3'
- *  else abort
- *
- *
- * Evidently, one instruction resolves to multiple
- * actions, though of course this is redundant as it was
- * already mentioned and demonstrated. The reason for
- * such enforcement is due to the definition of
- * "instruction" in Assembly. In the end, instructions
- * as we define them are groups of Assembly instructions
- * that would be required for a single C statement.
+ * For information on Instruction structures, please
+ * see dev-notes/C-interpreter/Instruction.md
 */
 
 /*
