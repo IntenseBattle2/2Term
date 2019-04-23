659aeda7e707b39e221540bc0a0249e41b496312
ColinTNMP <IB2Member01@gmail.com>
2019-04-23 02:43:42 -0400

    Added bare-bones array index op code

--- fragments/runInstruction.c
+++ fragments/runInstruction.c
@@ -98,6 +98,86 @@ int getInum(char* action, int startPoint, int* retvar, struct Instruction given_
   return inum;
 }
 
+//setVar() - sets the target variable to the given value in the passed number, according to the variable's type.
+void setVar(struct Variable* target, long double number)
+{ 
+  //Now for probably the most painful process of this entire thing. How do you feel about MASSIVE copy-paste?
+  //Yeah, this and others, I AM GOING TO 100% MOVE TO A NEW SOURCE FILE. I just don't have time rn, ironically.
+  enum DataType newType = target->type & typeLong & typeChar & typeShort & typeInt & typeDLong & typeFloat & typeDouble;
+  char isUnsigned       = target->type & typeUnsigned;
+  switch(newType){
+    case typeChar:
+      if ( isUnsigned ) target->value.uchar = (unsigned char)number;
+      target->value.schar = (char)number;
+    break;
+    case typeShort:
+      if ( isUnsigned ) target->value.ushort = (unsigned short)number;
+      target->value.sshort = (short)number;
+    break;
+    case typeInt:
+      if ( isUnsigned ) target->value.uint = (unsigned int)number;
+      target->value.sint = (int)number;
+    break;
+    case typeLong:
+      if ( isUnsigned ) target->value.ulong = (unsigned long)number;
+      target->value.slong = (long)number;
+    break;
+    case typeDLong:
+      if ( isUnsigned ) target->value.udlong = (unsigned long long)number;
+      target->value.sdlong = (long long)number;
+    break;
+    case typeFloat:
+      target->value.sfloat = (float)number;
+    break;
+    case typeDouble:
+      target->value.sdouble = (double)number;
+    break;
+    case typeLong + typeDouble:
+      target->value.sldouble = (long double)number;
+    break;
+  }
+  return;
+}
+
+//getVar() - Retrieve the number stored in the target variable. Defaults to long double, must type-cast it down. If you wanna.
+// Note: You can retrieve the type with getType()
+long double getVar(struct Variable* target)
+{
+  enum DataType newType = target->type & typeLong & typeChar & typeShort & typeInt & typeDLong & typeFloat & typeDouble;
+  char isUnsigned       = target->type & typeUnsigned;
+  switch(newType){
+    case typeChar:
+      if ( isUnsigned ) return (long double)target->value.uchar;
+      return (long double)target->value.schar;
+    break;
+    case typeShort:
+      if ( isUnsigned ) return (long double)target->value.ushort;
+      return (long double)target->value.sshort;
+    break;
+    case typeInt:
+      if ( isUnsigned ) return (long double)target->value.sint;
+      return (long double)target->value.uint;
+    break;
+    case typeLong:
+      if ( isUnsigned ) return (long double)target->value.ulong;
+      return (long double)target->value.slong;
+    break;
+    case typeDLong:
+      if ( isUnsigned ) return (long double)target->value.udlong;
+      return (long double)target->value.sdlong;
+    case typeFloat:
+      return (long double)target->value.sfloat;
+    break;
+    case typeDouble:
+      return (long double)target->value.sdouble;
+    break;
+    case typeLong + typeDouble:
+      return target->value.sldouble;
+    break;
+  }
+  return;
+}
+
 //The main function: runInstruction()
 //  NOTE: In this stage of development, this function may contain data that is not yet defined.
 /* Returned int gives status number:
@@ -113,10 +193,11 @@ int runInstruction(struct Instruction given_instruction)
        given_instruction.block[0]       == 0 &&
        given_instruction.variable[0]    == 0 &&
        given_instruction.macro[0]       == 0    ) return -1; //ERR01: NULL_INSTRUCTION
-  
-  int i,j,c,n,r,tnum,inum;    // i,j,c: looping vars; tnum: temp number storage; inum = index number from instruction members; n,r: numbers/result index
+
+  int i,j,c,n=0,r=0,f=0,tnum=0,inum=0;    // i,j,c: looping vars; tnum: temp number storage; inum = index number from instruction members; n,r,f: numbers/result/finalNumbers index
   char* action; // Current action being viewed
   struct Variable* numbers = malloc(given_instruction.numbersLen * sizeof(struct Variable)); //For getting data to be used in math
+  double finalNumbers[given_instruction.numbersLen];
   result = malloc(given_instruction.resultsLen * sizeof(struct Variable));
   enum {
     none,                         //Placeholder for null
@@ -124,14 +205,12 @@ int runInstruction(struct Instruction given_instruction)
     eqt, net, lth, lte, gth, gte, //EQual To, Not Equal To, Less THan, Less Than or Equal to, Greater THan, Greater Than or Equal to
     lan, lor, not,                //Logical ANd, Logical OR, logical NOT
     ban, bor, bxo, bco, bls, brs, //Bitwise ANd, Bitwise OR, Bitwise XOr, Bitwise not (2's COmplement), Bitwise Left Shift, Bitwise Right Shift
-    ass, szf, add, con,           //ASSignment, SiZeoF, ADDress, CONditional (ternary)
+    ass, szf, adr, con,           //ASSignment, SiZeoF, ADdRess, CONditional (ternary)
     mem,                          //MEMber operator
     ind = 100                     //INDirection -- made so that other values can be combined; indirection's unique nature makes it unable to be done by itself.
   } operator;
   enum DataType typeconv; //For knowing what to convert the type to during type conversion.
-        
     
-  
   // In these FOR loops, the given variables can never be -1, therefore they'll loop until a `break`
   for(c=0; c!=-1; c++)
   {
@@ -165,10 +244,11 @@ int runInstruction(struct Instruction given_instruction)
         break;
         case 'v':
           inum = getInum(action, i+1, &i, given_instruction);
-          numbers[n++] = variable[given_instruction.variable[inum]];
+          numbers[n++] = variable[given_instruction.variable[inum]]; //To note: since this can be any variable, incl a pointer, note longs are used to store addresses.
         break;
         case 'm':
           //TODO: Figure out how the heck macros fit into this since preprocessor stuff should be over...
+            //Solution: They don't fit, I'm a dunce, and I should stop thinking past 1am.
         break;
         case 'd':
           inum = getInum(action, i+1, &i, given_instruction);
@@ -188,9 +268,34 @@ int runInstruction(struct Instruction given_instruction)
           //TODO: Determine difference from arguments and type
         break;
         case '[': //for: a[x]
-          //TODO: Determine number or number variable
+          switch(action[i+1]){
+            case '0'||'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9':
+              tnum = stringToNumber(action, i+1, &i);
+              //Although we shouldn't, we're currently assuming the given variable must be an array.
+              //Of course, that means we're assuming the programmer knows what they're doing.
+              //OH, that also means we're assuming it's a pointer, therefore we're assuming the number is stored in a long. Prepare for crash
+            break;
+            case 'v':
+              inum = getInum(action, i+1, &i, given_instruction);
+              tnum = getVar(variable[given_instruction.variable[inum]]);
+            break;
+            case 'm':
+              inum = getInum(action, i+1, &i, given_instruction);
+              tnum = getVar(macro[given_instruction.macro[inum]]);
+            break;
+            case 'd':
+              inum = getInum(action, i+1, &i, given_instruction);
+              tnum = getVar(given_instruction.internal[inum]]);
+            break;
+            case 'r':
+              inum = getInum(action, i+1, &i, given_instruction);
+              tnum = getVar(result[inum]);
+            break;
+          }
+          result[r].type = number[n-1][tnum].type - typePointer;
+          setVar(&result[r], getVar(number[n-1][tnum]));
         break;
-        case '+': //For: a+b
+        case '+': //For: a+b, a++ and ++a don't exist here
           operator = add;
         break;
         case '-': //For: a->b, a-b
